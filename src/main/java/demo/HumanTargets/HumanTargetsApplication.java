d4062680d3547c2cca956b62330bac9b2ef802a7
Aryan <aryanbansalaryan@gmail.com>
2025-02-20 13:17:22 +0530

    first

---
+++ src/main/java/demo/HumanTargets/HumanTargetsApplication.java
@@ -0,0 +1,13 @@
+package demo.HumanTargets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanTargetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanTargetsApplication.class, args);
+	}
+
+}
